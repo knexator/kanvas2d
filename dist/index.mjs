55e0f6cea00605d280d56efaad1e8d7284e3bac4
unknown <daniel.hurtadoramirez314@gmail.com>
2024-01-14 12:31:07 +0100

    add twgl.js

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,5 +1,7 @@
 // src/index.ts
+import * as twgl from "twgl.js";
 function hello(x) {
+  console.log(twgl.addExtensionsToContext);
   return x * 2;
 }
 export {
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import * as twgl from "twgl.js"
+
 /**
  * example function
  * @param x cool number
  * @returns cooler number
  */
 export function hello(x: number): number {
+    console.log(twgl.addExtensionsToContext);
     return x * 2;
 }
